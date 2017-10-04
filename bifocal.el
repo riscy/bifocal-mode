a8b222b069a6bd64531b4780905989797bad8abe
riscy <chris@dissolve.com>
2017-10-04 12:18:54 -0600

    bump to version 0.0.3

--- bifocal.el
+++ bifocal.el
@@ -5,7 +5,7 @@
 ;; Keywords: frames, processes, tools
 ;; URL: https://github.com/riscy/bifocal-mode
 ;; Package-Requires: ((emacs "24.4"))
-;; Version: 0.0.1
+;; Version: 0.0.3
 
 ;; This file is NOT part of GNU Emacs.
 

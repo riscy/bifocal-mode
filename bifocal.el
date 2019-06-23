91d019b22b6420f7fd49fe6a4aa7ebab7f206dd1
riscy <riscy@users.noreply.github.com>
2019-06-23 16:35:55 -0600

    prefer https everywhere

--- bifocal.el
+++ bifocal.el
@@ -19,8 +19,8 @@
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
 
-;; You should have received a copy of the GNU General Public License
-;; along with this file.  If not, see <http://www.gnu.org/licenses/>.
+;; You should have received a copy of the GNU General Public License along with
+;; this file.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 

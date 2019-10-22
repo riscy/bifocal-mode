d7b1ec95cd87334472ed4b61d7bd600d82f6aa5a
riscy <riscy@users.noreply.github.com>
2019-10-21 20:42:27 -0700

    use SPDX-style license; upd email address

--- bifocal.el
+++ bifocal.el
@@ -1,22 +1,12 @@
 ;;; bifocal.el --- Split-screen scrolling for comint-mode buffers -*- lexical-binding: t -*-
 
-;; Authors: Chris Rayner (dchrisrayner @ gmail)
+;; Authors: Chris Rayner (dchrisrayner@gmail.com)
 ;; Created: May 23 2011
 ;; Keywords: frames, processes
 ;; URL: https://github.com/riscy/bifocal-mode
+;; SPDX-License-Identifier: GPL-3.0-or-later
 ;; Package-Requires: ((emacs "24.4"))
 ;; Version: 0.0.5
-
-;; This file is free software; you can redistribute it and/or modify it under
-;; the terms of the GNU General Public License as published by the Free Software
-;; Foundation; either version 3, or (at your option) any later version.
-
-;; This file is distributed in the hope that it will be useful, but WITHOUT ANY
-;; WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-;; A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License along with
-;; this file.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
